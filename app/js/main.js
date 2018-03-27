18021c54a670e4909f8c602264ec7187dbf0e7c9
Osster <_oster_@mail.ru>
2018-03-27 08:14:40 +0300

    initial commit

---
+++ app/js/main.js
@@ -0,0 +1,31 @@
+// Импортируем jQuery
+//= ../../node_modules/jquery/dist/jquery.min.js
+
+// Импортируем Popper
+//= popper.js
+
+// Импортируем необходимые js-файлы Bootstrap 4
+//= ../../node_modules/bootstrap/js/dist/util.js
+//= ../../node_modules/bootstrap/js/dist/alert.js
+//= ../../node_modules/bootstrap/js/dist/button.js
+//= ../../node_modules/bootstrap/js/dist/carousel.js
+//= ../../node_modules/bootstrap/js/dist/collapse.js
+//= ../../node_modules/bootstrap/js/dist/dropdown.js
+//= ../../node_modules/bootstrap/js/dist/modal.js
+//= ../../node_modules/bootstrap/js/dist/scrollspy.js
+//= ../../node_modules/bootstrap/js/dist/tab.js
+//= ../../node_modules/bootstrap/js/dist/tooltip.js
+//= ../../node_modules/bootstrap/js/dist/popover.js
+
+// Импортируем необходимые js-файлы lightslider
+//= ../../node_modules/lightslider/dist/js/lightslider.js
+
+// Импортируем библиотеки для работы с формами
+//= ../../node_modules/jquery-mask-plugin/dist/jquery.mask.js
+//= ../../node_modules/jquery-validation/dist/jquery.validate.js
+
+// Форма вслывающего сообщения
+//= ../../node_modules/sweetalert2/dist/sweetalert2.js
+
+// Импортируем другие js-файлы
+//= init.js
